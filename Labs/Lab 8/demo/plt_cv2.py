c1d45a2384ac93ec385089465bc1c31e5c59be56
Johnny <johnny5rocks@gmail.com>
2023-05-06 20:05:08 -0400

    updated lab 8

--- Labs/Lab 8/demo/plt_cv2.py
+++ Labs/Lab 8/demo/plt_cv2.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 from pandas import *
 from scipy import stats
-from sklearn.model_selection import cross_val_predict
 from sklearn import linear_model
+from sklearn.model_selection import cross_val_predict
 
-X = read_csv('cpu_usage_temp.csv', usecols=[1])
+
+X = read_csv('cpu_usage_temp.csv', usecols=[3])
 X = X.assign(t = X.index)
 y = read_csv('cpu_usage_temp.csv', usecols=[2])
 lr = linear_model.LinearRegression()
@@ -13,6 +14,6 @@ plt.plot(y, predicted, 'ro')
 plt.plot([y.min(), y.max()], [y.min(), y.max()], 'b-', lw=2)
 plt.xlabel('Temperature C')
 plt.ylabel('Predicted')
-plt.title('Johnny's Laptop 2023-3-29')
+plt.title('John Laptop 2023-3-29')
 
 plt.show()
--- Labs/Lab 8/demo/plt_final.py
+++ Labs/Lab 8/demo/plt_final.py
@@ -9,10 +9,10 @@ y = data['Temperature C']
 # Time series
 plt.plot(y, 'r', lw=2, label='Temperature C')
 plt.plot(x, 'b', lw=2, label='CPU Usage %')
-plt.xticks([209,452,703,957],['21:00','21:30','22:00','22:30'])
+plt.xticks([209,452,703,957],['16:07','16:10','16:13','16:16'])
 plt.xlabel('Time')
 plt.legend(loc='lower center')
-plt.title('Johnny's Laptop 2023-3-29')
+plt.title('Johnnys Laptop 2023-3-29')
 
 # Histogram of CPU usage
 plt.figure()
@@ -20,7 +20,7 @@ num_bins = 35
 n, bins, patches = plt.hist(x, num_bins, density=1, facecolor='blue', alpha=0.5)
 plt.xlabel('CPU Usage %')
 plt.ylabel('Probability')
-plt.title('Johnny's Laptop 2023-3-29')
+plt.title('Johnnys Laptop 2023-3-29')
 
 # Histogram of temperature
 plt.figure()
@@ -28,19 +28,19 @@ num_bins = 30
 n, bins, patches = plt.hist(y, num_bins, density=1, facecolor='red', alpha=0.5)
 plt.xlabel('Temperature C')
 plt.ylabel('Probability')
-plt.title('Johnny's Laptop 2023-3-29')
+plt.title('Johnnys Laptop 2023-3-29')
 
 # Horizontal box plot of CPU usage
 plt.figure()
 plt.boxplot(x, 0, '+', 0)
 plt.xlabel('CPU Usage %')
-plt.title('Johnny's Laptop 2023-3-29')
+plt.title('Johnnys Laptop 2023-3-29')
 
 # Vertical box plot of temperature
 plt.figure()
 plt.boxplot(y, 0, '+')
 plt.ylabel('Temperature C')
-plt.title('Johnny's Laptop 2023-3-29')
+plt.title('Johnnys Laptop 2023-3-29')
 
 # Scatter diagram with a linear regression line
 plt.figure()
@@ -50,6 +50,6 @@ plt.ylabel('Temperature C')
 plt.plot(x, y, 'bo')
 l = [slope * i + intercept for i in x]
 plt.plot(x, l, 'r', lw=2)
-plt.title('Johnny's Laptop 2023-3-29')
+plt.title('Johnnys Laptop 2023-3-29')
 
-plt.show()s
+plt.show()
